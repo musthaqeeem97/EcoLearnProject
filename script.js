b5763ea41bd00f5788c76bca6b60b5af71ee2c63
Musthaqeem <musthaqeem97@gmail.com>
2024-01-13 19:13:16 +0900

    adding video links

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
